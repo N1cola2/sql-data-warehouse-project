c7022e4289d78ef7730606d2856a9cecd7d82605
N1cola2 <87427892+N1cola2@users.noreply.github.com>
2025-02-16 20:15:58 -0300

    Update and rename placeholder to init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,43 @@
+/*
+==============================================================================
+Criar a base de dados e os esquemas
+==============================================================================
+Propósito do Script:
+	Este script cria uma nova database chamada 'DataWarehouse' após checar se ele já existe.
+	Se a database existe, é deletada e recriada. Adicionalmente, o script deixa pronto três 
+	esquemas dentro da database: 'bronze','prata','ouro'.
+
+AVISO:
+	Rodar este script vai deletar a database criada por inteiro se a mesma existir.
+	Todos os dados dentro da database serão permanentemente deletados. Prossiga com cuidado
+	e se certifique de ter os backups apropriados antes de rodar este script.
+*/
+
+USE master;
+GO
+
+--Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases where name ='DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Cria a a database 'DataWarehouse'
+CREATE DATABASE DataWarehouse
+GO
+
+USE Datawarehouse;
+GO
+
+--Cria os Esquemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA prata;
+GO
+
+CREATE SCHEMA ouro;
+GO
+
